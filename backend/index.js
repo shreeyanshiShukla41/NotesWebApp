f11f379669a63761f0d3b432ba6fbfdb28632d8d
shreeyanshiShukla41 <166681385+shreeyanshiShukla41@users.noreply.github.com>
2025-11-16 21:26:29 +0530

    Update CORS configuration to allow all origins

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,9 @@ async function main() {
 }
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/", router);
